43f29a6c26b88ed725128e8b98bb7ecc081365d7
Rafael Sampaio <48326757+sampaiorg@users.noreply.github.com>
2020-05-09 19:05:20 -0300

    Update Product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MatSortModule } from '@angular/material/sort';
 //PARA ATRIBUIR O TIPO DE MOEDA AO PRICE
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 registerLocaleData(localePt);
 //TERMINA MOEDA
 
@@ -45,7 +46,8 @@ registerLocaleData(localePt);
     ProductCrudComponent,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
